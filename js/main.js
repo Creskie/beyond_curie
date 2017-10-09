e24e84348ea124a2daac0a6635fc63c4dddba432
Lisa Wong <squashedsquash@gmail.com>
2017-10-09 19:41:43 -0400

    no flip, colored divs pop up, font update, added text assets

--- js/main.js
+++ js/main.js
@@ -1,51 +1,445 @@
 //Click card back to fade in/out full-screen div
-$( "#back_1" ).click(function() {
-	$("#cover_one").fadeIn( "slow", function() {
+$("#card_1").click(function() {
+	$("#cover_1").fadeIn( "slow", function() {
   });
   //Disable scroll when full-screen div
 	$('body').addClass('stop-scrolling');
 	$('body').bind('touchmove', function(e){e.preventDefault()});
 });
-
-$("#cover_one").click(function() {
-  $("#cover_one").fadeOut( "slow", function() {
-  $(".card").flip(false);	
+$("#cover_1").click(function() {
+  $("#cover_1").fadeOut( "slow", function() {	
   });
   //Enable scroll when exiting full-screen div
   	$('body').removeClass('stop-scrolling');
   	$('body').unbind('touchmove');
 });
 
-//Loading Screen
-function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 3000);
-
-    function checkReady() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
-            window.clearInterval(intervalID);
-            callback.call(this);
-        }
-    }
-}
-
-function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none';
-}
-
-onReady(function () {
-    show('container', true);
-	$('#loading').fadeOut("slow");
+$("#card_2").click(function() {
+	$("#cover_2").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_2").click(function() {
+  $("#cover_2").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
 });
 
-//Card flip when clicked
-$(".card").flip();
+$("#card_3").click(function() {
+	$("#cover_3").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_3").click(function() {
+  $("#cover_3").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
 
-//Disable flip once flipped
-// $('.card').on('flip:done', function onFlipDone() {
-// 	$(this).off('.flip');
-// });
+$("#card_4").click(function() {
+	$("#cover_4").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_4").click(function() {
+  $("#cover_4").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
 
-//Page position starts at top
+$("#card_5").click(function() {
+	$("#cover_5").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_5").click(function() {
+  $("#cover_5").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_6").click(function() {
+	$("#cover_6").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_6").click(function() {
+  $("#cover_6").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_7").click(function() {
+	$("#cover_7").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_7").click(function() {
+  $("#cover_7").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_8").click(function() {
+	$("#cover_8").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_8").click(function() {
+  $("#cover_8").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_9").click(function() {
+	$("#cover_9").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_9").click(function() {
+  $("#cover_9").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_10").click(function() {
+	$("#cover_10").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_10").click(function() {
+  $("#cover_10").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_11").click(function() {
+	$("#cover_11").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_11").click(function() {
+  $("#cover_11").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_12").click(function() {
+	$("#cover_12").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_12").click(function() {
+  $("#cover_12").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_13").click(function() {
+	$("#cover_13").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_13").click(function() {
+  $("#cover_13").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_14").click(function() {
+	$("#cover_14").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_14").click(function() {
+  $("#cover_14").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_15").click(function() {
+	$("#cover_15").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_15").click(function() {
+  $("#cover_15").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_16").click(function() {
+	$("#cover_16").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_16").click(function() {
+  $("#cover_16").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_17").click(function() {
+	$("#cover_17").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_17").click(function() {
+  $("#cover_17").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_18").click(function() {
+	$("#cover_18").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_18").click(function() {
+  $("#cover_18").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_19").click(function() {
+	$("#cover_19").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_19").click(function() {
+  $("#cover_19").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_20").click(function() {
+	$("#cover_20").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_20").click(function() {
+  $("#cover_20").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_21").click(function() {
+	$("#cover_21").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_21").click(function() {
+  $("#cover_21").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_22").click(function() {
+	$("#cover_22").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_22").click(function() {
+  $("#cover_22").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_23").click(function() {
+	$("#cover_23").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_23").click(function() {
+  $("#cover_23").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_24").click(function() {
+	$("#cover_24").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_24").click(function() {
+  $("#cover_24").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_25").click(function() {
+	$("#cover_25").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_25").click(function() {
+  $("#cover_25").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_26").click(function() {
+	$("#cover_26").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_26").click(function() {
+  $("#cover_26").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_27").click(function() {
+	$("#cover_27").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_27").click(function() {
+  $("#cover_27").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+$("#card_28").click(function() {
+	$("#cover_28").fadeIn( "slow", function() {
+  });
+  //Disable scroll when full-screen div
+	$('body').addClass('stop-scrolling');
+	$('body').bind('touchmove', function(e){e.preventDefault()});
+});
+$("#cover_28").click(function() {
+  $("#cover_28").fadeOut( "slow", function() {	
+  });
+  //Enable scroll when exiting full-screen div
+  	$('body').removeClass('stop-scrolling');
+  	$('body').unbind('touchmove');
+});
+
+// Loading Screen
+(function setupLoadingAnimation() {
+	var shouldStop = false;
+
+	function stopLoadingAnimation() {
+		$('#container').css('display', 'block');
+		$('#loading').fadeOut("slow");
+	};
+
+	function detectRace() {
+		if (shouldStop) {
+			stopLoadingAnimation();
+		}
+		shouldStop = true;
+	};
+
+	window.setTimeout(detectRace, 2000);
+	$(document).ready(detectRace);
+})();
+
+// Page position starts at top
 if ('scrollRestoration' in history) {
 	history.scrollRestoration = 'manual';
 }
@@ -61,7 +455,7 @@ $("li a").click(function(event){
 	$("html, body").animate({scrollTop: divPosition});
 });
 
-//Side navigation bar fade while scrolling
+// Side navigation bar fade while scrolling
 (function setupSidebar() {
 	var lastTimeoutHandle = null;
 	var shouldFadeOut = true;
